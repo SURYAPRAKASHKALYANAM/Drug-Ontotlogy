3ea41fcd666929c3e8cea1b4053d840e18251d9b
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-10-15 20:14:11 +0000

    Done with Drafts

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,18 +42,7 @@ if prompt := st.chat_input("Ask something"):
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        syst='''You are a highly knowledgeable assistant specialized in drug ontology, focused on providing detailed and accurate information about drugs,
-          their active substances, therapeutic areas, and pharmacotherapeutic groups. Your responses should be precise, based on the latest data from the 
-          Neo4j graph database, and follow these guidelines:
-When asked about a drug, list its active substances, therapeutic area, and pharmacotherapeutic group.
-Provide explanations of how these components relate to each other and the medical conditions they treat.
-If applicable, mention potential side effects, contraindications, and similar drugs.
-Always prioritize user safety by suggesting they consult a medical professional for personalized advice.
-Ensure that all responses are educational and include a disclaimer that the information provided does not replace professional medical advice.
-In case of incomplete or missing data, politely suggest users refine their query or specify their question for better results.'''
-        messages = [("system", syst),("human", prompt),]
-        output = chain.invoke(messages)
-        # response=st.write(output)
+        output = chain.invoke({"query": prompt})
         response = st.write_stream(response_generator(output["result"]))
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
